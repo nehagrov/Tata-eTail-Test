a421fcdb0fc143c6c14654d6a8c49e5b35f946df
131005 <dbhargava@ezibuy.co.nz>
2015-02-25 15:08:13 +0530

    commit

---
+++ trainingcockpits/gensrc/org/training/cockpits/constants/GeneratedTrainingCockpitsConstants.java
@@ -0,0 +1,24 @@
+/*
+ * ----------------------------------------------------------------
+ * --- WARNING: THIS FILE IS GENERATED AND WILL BE OVERWRITTEN! ---
+ * --- Generated at 25 Feb, 2015 2:51:04 PM                     ---
+ * ----------------------------------------------------------------
+ */
+package org.training.cockpits.constants;
+
+/**
+ * @deprecated use constants in Model classes instead
+ */
+@Deprecated
+@SuppressWarnings({"unused","cast","PMD"})
+public class GeneratedTrainingCockpitsConstants
+{
+	public static final String EXTENSIONNAME = "trainingcockpits";
+	
+	protected GeneratedTrainingCockpitsConstants()
+	{
+		// private constructor
+	}
+	
+	
+}
